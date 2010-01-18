8a4922e8f5e017af71952061858e74070d5e5f1d
Rafael Fernández López <ereslibre@ereslibre.es>
2010-01-18 18:50:28 +0100

    Mejora la fuerza de la pelota

--- igr/practica3/pelota.cpp
+++ igr/practica3/pelota.cpp
@@ -36,6 +36,9 @@ GLdouble Pelota::getRadio() const
 void Pelota::incrementaFuerza()
 {
     m_fuerza += 0.5;
+    if (m_fuerza == 50.0) {
+        m_fuerza = 0;
+    }
 }
 
 void Pelota::incrementaAngulo()
