3965b783241f13daa145cecd7c1362e947cd3ae9
Rafael Fernández López <ereslibre@ereslibre.es>
2010-01-19 18:47:30 +0100

    Dibuja normales bien !!

--- igr/practica3/poligonoconvexo.cpp
+++ igr/practica3/poligonoconvexo.cpp
@@ -46,29 +46,32 @@ void PoligonoConvexo::dibujaEnvoltorio(Lapiz &lapiz) const
 
 void PoligonoConvexo::dibujaNormales(Lapiz &lapiz) const
 {
-//     const GLdouble alpha = 2.0 * M_PI / (GLdouble) m_nLados;
-//     const GLdouble beta = (M_PI - alpha) / 2.0;
-//     const GLdouble gamma = M_PI - beta;
-//     const GLdouble radio = ((m_lado + RADIO_PELOTA * 2.0) / 2.0) / cos(beta);
-//     PV2f pos(m_pos.getX() + radio, m_pos.getY());
-//     lapiz.setPos(pos);
-//     lapiz.girar(gamma);
-//     for (size_t i = 0; i < m_nLados; ++i) {
-//         const PV2f oldPos = lapiz.getPos();
-//         lapiz.avanzar((m_lado + RADIO_PELOTA * 2.0) / 2.0, Lapiz::NoDejarRastro);
-//         const PV2f medPos = lapiz.getPos();
-//         lapiz.avanzar((m_lado + RADIO_PELOTA * 2.0) / 2.0, Lapiz::NoDejarRastro);
-//         const PV2f pos = lapiz.getPos();
-//         PV2f aux = oldPos - pos;
-//         aux = aux.normal(PV2f::Izquierda);
-//         aux.normalizar();
-//         aux *= RADIO_PELOTA;
-//         glBegin(GL_LINES);
-//         glVertex2d(medPos.getX(), medPos.getY());
-//         glVertex2d((aux + medPos).getX(), (aux + medPos).getY());
-//         glEnd();
-//         lapiz.girar(alpha);
-//     }
+    const GLdouble alpha = 2.0 * M_PI / (GLdouble) m_nLados;
+    const GLdouble b = RADIO_PELOTA;
+    const GLdouble c = b / cos(alpha / 2.0);
+    const GLdouble a = c * sin(alpha / 2.0);
+    const GLdouble beta = (M_PI - alpha) / 2.0;
+    const GLdouble gamma = M_PI - beta;
+    const GLdouble radio = (m_lado / 2.0) / cos(beta) + c;
+    PV2f pos(m_pos.getX() + radio, m_pos.getY());
+    lapiz.setPos(pos);
+    lapiz.girar(gamma);
+    for (size_t i = 0; i < m_nLados; ++i) {
+        const PV2f oldPos = lapiz.getPos();
+        lapiz.avanzar((m_lado + 2 * a) / 2.0, Lapiz::NoDejarRastro);
+        const PV2f medPos = lapiz.getPos();
+        lapiz.avanzar((m_lado + 2 * a) / 2.0, Lapiz::NoDejarRastro);
+        const PV2f pos = lapiz.getPos();
+        PV2f aux = oldPos - pos;
+        aux = aux.normal(PV2f::Izquierda);
+        aux.normalizar();
+        aux *= RADIO_PELOTA;
+        glBegin(GL_LINES);
+        glVertex2d(medPos.getX(), medPos.getY());
+        glVertex2d((aux + medPos).getX(), (aux + medPos).getY());
+        glEnd();
+        lapiz.girar(alpha);
+    }
 }
 
 bool PoligonoConvexo::colisiona(const Pelota &pelota, GLdouble &thit, PV2f &n)
